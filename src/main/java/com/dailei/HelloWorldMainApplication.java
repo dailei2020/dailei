fe52840e0a1f4f31ca1996c91984ae1c7342d29a
戴磊 <982238845@qq.com>
2020-05-07 15:52:55 +0800

    Initial commit

---
+++ src/main/java/com/dailei/HelloWorldMainApplication.java
@@ -0,0 +1,10 @@
+package com.dailei;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
